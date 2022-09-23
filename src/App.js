3ba3e1def8e57ff03ea5116a95f82533d31b24b6
Nikhilmehra1302 <106437886+Nikhilmehra1302@users.noreply.github.com>
2022-09-23 17:41:00 +0530

    Here's the Commint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+/*import "./styles.css";
 
 export default function App() {
   return (
@@ -9,4 +9,4 @@ export default function App() {
       </div>
     </>
   );
-}
+}*/
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+/*(1) Importing react 1st way */
+/*
+let React = require("react");
+let ReactDOM = require("react-dom");
+*/
+/* Importing react 2nd way*/
+import React from "react";
+import ReactDOM from "react-dom";
+
+var kDOt = "I can't Please Everybody\n -k Dot";
+const root = document.getElementById("root");
+ReactDOM.render(
+  <>
+    <h1>{kDOt}</h1>
+  </>,
+  root
+);
+
+/* (3) with Javsscript */
+let h1 = document.createElement("h2");
+h1.innerHTML = "Sleep";
+root.appendChild(h1);
+
+/*Best way to add React in js (4)
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
@@ -10,4 +34,4 @@ root.render(
   <StrictMode>
     <App />
   </StrictMode>
-);
+);*/
