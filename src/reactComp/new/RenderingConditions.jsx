f2756820ddb645d01a837266b9f67b89a749502a
Nikhilmehra1302 <106437886+Nikhilmehra1302@users.noreply.github.com>
2022-10-08 23:49:04 +0530

    Commit

--- src/reactComp/new/RenderingConditions.jsx
+++ src/reactComp/new/RenderingConditions.jsx
@@ -2,16 +2,17 @@ function Inputt(props) {
   return (
     <form className="form">
       <input type={props.type} placeholder={props.typeHolder} />
+      <button type="submit">{props.message}</button>
     </form>
   );
 }
 
+var isRegistered = false;
 export default function Login() {
   return (
     <div className="formContainer">
       <Inputt type="text" typeHolder="Username" />
       <Inputt type="text" typeHolder="password" />
-      <button type="submit">Register</button>
     </div>
   );
 }
